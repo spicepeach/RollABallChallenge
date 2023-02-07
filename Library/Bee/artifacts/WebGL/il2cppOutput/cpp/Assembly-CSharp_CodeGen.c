1a30926150ffbd2348c203d74fecd11f9f28020d
spicepeach <89557364+spicepeach@users.noreply.github.com>
2023-02-07 15:55:46 -0500

    Publish

---
+++ Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,85 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void CameraController::Start()
+extern void CameraController_Start_m8EAAC188934ECCBD009EC00751280DCFF4D96C71 (void);
+// 0x00000002 System.Void CameraController::LateUpdate()
+extern void CameraController_LateUpdate_mC14DB0924E6F67288113AA92B3345A9B0ACEA27F (void);
+// 0x00000003 System.Void CameraController::.ctor()
+extern void CameraController__ctor_mE196A6332BDDED632D6F9DB6260E424594598950 (void);
+// 0x00000004 System.Void PlayerController::Start()
+extern void PlayerController_Start_m1D83076E8B136A71051F2F02545EE04947D3A8CF (void);
+// 0x00000005 System.Void PlayerController::FixedUpdate()
+extern void PlayerController_FixedUpdate_m6D906D8B13844542B81CC49BA19760F747CEC8C0 (void);
+// 0x00000006 System.Void PlayerController::OnTriggerEnter(UnityEngine.Collider)
+extern void PlayerController_OnTriggerEnter_mF2704872AEA62333341DCBEA2831320C385701D1 (void);
+// 0x00000007 System.Void PlayerController::OnMove(UnityEngine.InputSystem.InputValue)
+extern void PlayerController_OnMove_m26350714C4683F0DAE28FDBBB49112254AF9E2B1 (void);
+// 0x00000008 System.Void PlayerController::SetCountText()
+extern void PlayerController_SetCountText_m6A9F5CF284E511EBFE348D2500F866BAB8CE2C24 (void);
+// 0x00000009 System.Void PlayerController::.ctor()
+extern void PlayerController__ctor_mDDAB7C7D82E1A5B3E6C197B1AB9D653DFE554F33 (void);
+// 0x0000000A System.Void Rotater::Update()
+extern void Rotater_Update_m150F6D32E9F756C67EBF03916564B47F648AB85F (void);
+// 0x0000000B System.Void Rotater::.ctor()
+extern void Rotater__ctor_m30F6739C915DA85AC365716BB3CCD7CAFC774632 (void);
+static Il2CppMethodPointer s_methodPointers[11] = 
+{
+	CameraController_Start_m8EAAC188934ECCBD009EC00751280DCFF4D96C71,
+	CameraController_LateUpdate_mC14DB0924E6F67288113AA92B3345A9B0ACEA27F,
+	CameraController__ctor_mE196A6332BDDED632D6F9DB6260E424594598950,
+	PlayerController_Start_m1D83076E8B136A71051F2F02545EE04947D3A8CF,
+	PlayerController_FixedUpdate_m6D906D8B13844542B81CC49BA19760F747CEC8C0,
+	PlayerController_OnTriggerEnter_mF2704872AEA62333341DCBEA2831320C385701D1,
+	PlayerController_OnMove_m26350714C4683F0DAE28FDBBB49112254AF9E2B1,
+	PlayerController_SetCountText_m6A9F5CF284E511EBFE348D2500F866BAB8CE2C24,
+	PlayerController__ctor_mDDAB7C7D82E1A5B3E6C197B1AB9D653DFE554F33,
+	Rotater_Update_m150F6D32E9F756C67EBF03916564B47F648AB85F,
+	Rotater__ctor_m30F6739C915DA85AC365716BB3CCD7CAFC774632,
+};
+static const int32_t s_InvokerIndices[11] = 
+{
+	4433,
+	4433,
+	4433,
+	4433,
+	4433,
+	3534,
+	3534,
+	4433,
+	4433,
+	4433,
+	4433,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	11,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
